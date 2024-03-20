dea953dc370edb403625ce6541b5e125b2c2e7ef
jhyunleehi <jhyunleehi@naver.com>
2024-03-20 11:18:49 +0000

    ex

--- ex03-file/filelife.c
+++ ex03-file/filelife.c
@@ -13,6 +13,22 @@
 #define DNAME_INLINE_LEN	32
 #define TASK_COMM_LEN		16
 
+enum op {
+  CREATE,
+  OPEN,
+  INODE,
+  UNLINK,
+};
+
+struct fevent {  
+  __u32 pid;
+  __u32 tid;  
+  char file[DNAME_INLINE_LEN];
+  char task[TASK_COMM_LEN];  
+  enum op op;
+};
+
+
 struct event {
 	char file[DNAME_INLINE_LEN];
 	char task[TASK_COMM_LEN];
@@ -22,6 +38,26 @@ struct event {
 	//void *dentry;
 	__u64 dentry;
 };
+
+//__attribute__((unused))는 C나 C++에서 사용되는 컴파일러 지시자입니다. 
+//이 지시자는 컴파일러에게 해당 변수나 함수가 사용되지 않을 것임을 알려줍니다. 
+//주로 개발자가 의도적으로 변수나 함수를 선언하지만 사용하지 않을 때 사용됩니다. 
+//이렇게 하면 컴파일러는 해당 변수나 함수를 사용하지 않은 것으로 간주하고, 
+//이에 따른 경고 메시지를 표시하지 않게 됩니다.
+const struct event *unused __attribute__((unused));
+
+struct {
+  __uint(type, BPF_MAP_TYPE_RINGBUF);
+  __uint(max_entries, 1 << 24);
+} fevents SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, sizeof(u32));
+} events SEC(".maps");
+
+
 
 struct renamedata___x {
 	struct user_namespace *old_mnt_userns;
@@ -42,16 +78,7 @@ static __always_inline bool renamedata_has_new_mnt_idmap_field(void)
 	return false;
 }
 
-
 const volatile pid_t targ_tgid = 0;
-
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 8192);
-	__type(key, u64);
-	__type(value,u64);
-} filemap SEC(".maps");
-
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
@@ -60,18 +87,7 @@ struct {
 	__type(value, u64);
 } start SEC(".maps");
 
-struct {
-	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-	__uint(key_size, sizeof(u32));
-	__uint(value_size, sizeof(u32));
-} events SEC(".maps");
 
-//__attribute__((unused))는 C나 C++에서 사용되는 컴파일러 지시자입니다. 
-//이 지시자는 컴파일러에게 해당 변수나 함수가 사용되지 않을 것임을 알려줍니다. 
-//주로 개발자가 의도적으로 변수나 함수를 선언하지만 사용하지 않을 때 사용됩니다. 
-//이렇게 하면 컴파일러는 해당 변수나 함수를 사용하지 않은 것으로 간주하고, 
-//이에 따른 경고 메시지를 표시하지 않게 됩니다.
-const struct event *unused __attribute__((unused));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
@@ -95,6 +111,38 @@ static __always_inline int probe_create(struct dentry *dentry)
 	return 0;
 }
 
+static int submit_fevent(enum op op, struct dentry *dentry ) 
+{  
+	
+	__u64 pid_tgid = bpf_get_current_pid_tgid();
+  	__u32 pid = pid_tgid >> 32;
+  	__u32 tid = (__u32)pid_tgid; 
+
+	bpf_printk("DEBUG: submit_fevent===>> [%d]", pid);
+
+	struct fevent *feventp;
+	feventp = bpf_ringbuf_reserve(&fevents, sizeof(struct fevent), 0);
+  	if (!feventp) {
+	    return 0;
+  	}
+
+
+  	feventp->pid = pid;
+  	feventp->tid = tid;    
+  	feventp->op = op;
+  	bpf_get_current_comm(&feventp->task, sizeof(feventp->task));
+	if (dentry) {
+		const u8 *qs_name_ptr =	BPF_CORE_READ(dentry, d_name.name);
+		bpf_probe_read_kernel_str(&feventp->file, sizeof(feventp->file), qs_name_ptr);
+		//bpf_probe_read_kernel_str(&fevent.file, sizeof(fevent.file), dentry->d_name.name);
+	}
+	bpf_ringbuf_submit(feventp, 0);
+  
+  return 0;
+}
+
+
+
 /**
  * In different kernel versions, function vfs_create() has two declarations,
  * and their parameter lists are as follows:
@@ -109,10 +157,14 @@ static __always_inline int probe_create(struct dentry *dentry)
 SEC("kprobe/vfs_create")
 int BPF_KPROBE(vfs_create, void *arg0, void *arg1, void *arg2){
 //int  kprobe_vfs_create(struct pt_regs *ctx) {
-	if (renamedata_has_old_mnt_userns_field() || renamedata_has_new_mnt_idmap_field())
+	if (renamedata_has_old_mnt_userns_field() || renamedata_has_new_mnt_idmap_field()){
 		probe_create(arg2);
-	else
+		submit_fevent(CREATE, arg2);
+	}
+	else{
 		probe_create(arg1);
+		submit_fevent(CREATE, arg1);
+	}
 
 	int pid = bpf_get_current_pid_tgid() >> 32;
   	const char fmt_str[] = "VFS create [%d]";
@@ -136,6 +188,8 @@ int BPF_KPROBE(vfs_open, struct path *path, struct file *file)
   	const char fmt_str[] = "VFS open [%d]";
   	bpf_trace_printk(fmt_str, sizeof(fmt_str), pid);
 
+	submit_fevent(OPEN, dentry);
+
 	return probe_create(dentry);
 }
 
@@ -143,6 +197,8 @@ SEC("kprobe/security_inode_create")
 int BPF_KPROBE(security_inode_create, struct inode *dir,
 	     struct dentry *dentry)
 {
+	submit_fevent(INODE, dentry);
+
 	return probe_create(dentry);
 }
 
@@ -179,7 +235,7 @@ int BPF_KPROBE(vfs_unlink, void *arg0, void *arg1, void *arg2)
 
 	qs_name_ptr = has_arg
 		? BPF_CORE_READ((struct dentry *)arg2, d_name.name)
-		: BPF_CORE_READ((struct dentry *)arg1, d_name.name);
+		: BPF_CORE_READ((struct dentry *)arg1, d_name.name);    		
 
 	bpf_probe_read_kernel_str(&event.file, sizeof(event.file), qs_name_ptr);
 	bpf_get_current_comm(&event.task, sizeof(event.task));
@@ -193,7 +249,12 @@ int BPF_KPROBE(vfs_unlink, void *arg0, void *arg1, void *arg2)
   	const char fmt_str[] = "VFS ulink [%d]";
   	bpf_trace_printk(fmt_str, sizeof(fmt_str), pid);
 
-	bpf_map_update_elem(&filemap, &event.dentry, &event.file, BPF_ANY);
+
+	if (has_arg ){		
+		submit_fevent(UNLINK, arg2);
+	}else{
+		submit_fevent(UNLINK, arg1);
+	}
 
 	return 0;
 }
